f41efc44730c75f849e9ae767adc6ebb3c343890
Axel Ahlen <Axel.ahlen@skola.taby.se>
2025-10-23 13:01:25 +0000

    Gissa_talet.py

--- Gissa_talet.py
+++ Gissa_talet.py
@@ -1,13 +1,3 @@
-#name = (input("Vad heter du?  "))
-#print("Hej", (name), "och välkommen till spelet!")
-
-#import random as rand
-#tal = int(input("Gissa ett tal mellan 1 och 10: "))
-#thing = rand.randint(1, 10)
-#if thing == tal:
-#    print("Jippi, det var", (thing))
-#else:
-#    print("Tyvärr, det var",(thing))
 import random as rand
 thing = rand.randint(1, 10)
 for round_ in range(1, 3 +1):
@@ -15,6 +5,11 @@ for round_ in range(1, 3 +1):
     if tal == thing:
         print("Rätt gissat!")
         break
+    elif tal < thing:
+        print("Talet är större än", (tal))
+    elif tal > thing:
+        print("Talet är mindre än", (tal))
     else:
         print("Fel gissat.")
 print("talet var", (thing))
+print("Antal gissningar:", (round_))
