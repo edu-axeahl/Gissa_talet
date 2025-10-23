4154d7d3525467f411490ff9e0d09e8b9e3d2eeb
Axel Ahlen <Axel.ahlen@skola.taby.se>
2025-10-23 12:41:14 +0000

    Gissa_talet.py

--- Gissa_talet.py
+++ Gissa_talet.py
@@ -1,2 +1,10 @@
-name = (input("Vad heter du?  "))
-print("Hej", (name), "och välkommen till spelet!")
+#name = (input("Vad heter du?  "))
+#print("Hej", (name), "och välkommen till spelet!")
+
+import random as rand
+tal = int(input("Gissa ett tal mellan 1 och 10: "))
+thing = rand.randint(1, 10)
+if thing == tal:
+    print("Jippi, det var", (thing))
+else:
+    print("Tyvärr, det var",(thing))
